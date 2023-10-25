d34a1bf2107617ccff8b8bbff6cbadcda581705a
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-25 12:33:36 +0600

    basic structure added for port 5000

---
+++ index.js
@@ -0,0 +1,9 @@
+// import
+const express = require('express');
+
+const app = express();
+
+// port listening
+app.listen(5000, () => {
+    console.log("vounteer-network-server is listening to port: 5000");
+});
