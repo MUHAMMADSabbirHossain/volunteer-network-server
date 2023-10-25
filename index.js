09ff2be48a04d0eac5bf6b29ae11bcabfd5ccafe
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-25 12:38:57 +0600

    server home page is added successfully.

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send(`vounteer-network-server is listening to port: ${port}`);
+})
+
 // port listening
 app.listen(port, () => {
     console.log("vounteer-network-server is listening to port: ", port);
