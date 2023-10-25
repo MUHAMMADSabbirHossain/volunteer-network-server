e92f08855dcba732c8e670c3ffb8c89aa91cdd6e
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-25 12:42:02 +0600

    cors & express.json middleware added.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // import
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send(`vounteer-network-server is listening to port: ${port}`);
